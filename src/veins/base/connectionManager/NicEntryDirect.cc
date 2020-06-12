40b037d7f54447008e279afcc005c4f5d21aa6d6
Michele Segata <segata@ccs-labs.org>
2020-06-12 16:28:50 +0200

    Nic80211p: rename direct in gate to veins11pRadioIn to enable a vehicle to use multiple heterogeneous technologies

--- src/veins/base/connectionManager/NicEntryDirect.cc
+++ src/veins/base/connectionManager/NicEntryDirect.cc
@@ -38,7 +38,7 @@ void NicEntryDirect::connectTo(NicEntry* other)
     EV_TRACE << "connecting nic #" << nicId << " and #" << other->nicId << endl;
 
     cGate* radioGate = nullptr;
-    if ((radioGate = otherPtr->gate("radioIn")) == nullptr) throw cRuntimeError("Nic has no radioIn gate!");
+    if ((radioGate = otherPtr->gate("veins11pRadioIn")) == nullptr) throw cRuntimeError("Nic has no veins11pRadioIn gate!");
 
     outConns[other] = radioGate->getPathStartGate();
 }
